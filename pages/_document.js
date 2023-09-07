18d9cec90bada1f0fc3667edfdabddf1d96395db
wills_dev <godswillijezei@gmail.com>
2023-09-07 11:51:28 +0100

    ui changes

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <title>PikaBoo Waste Management</title>
         <meta
           property="og:title"
           content="PikaBoo Waste Management"
